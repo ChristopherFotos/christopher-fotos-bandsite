d7ac21d1dd1411a2db85e4ddafe8e93cc6e6eb2e
Chris Johns <chrisfotosmusic@gmail.com>
2020-12-01 18:45:19 -0500

    add submit comments with axios post request

--- sprint-two/scripts/comments.js
+++ sprint-two/scripts/comments.js
@@ -5,64 +5,62 @@ let submitButton     = document.getElementById('add-comment');
 
 
 
-// create comment element
+/* create comment element */
 function displayComment(comment){
 
-    //create comment div
+    /* displayComment function: create comment div */
     let commentDiv = document.createElement('div');
     commentDiv.classList.add('comments__content');
 
-    // create avatar and append it to comment div
+    /* displayComment function: create avatar and append it to comment div */
     let commentImg = document.createElement('img');
     commentImg.classList.add('comments__avatar');
     commentImg.setAttribute('src', './assets/images/avatars/1.jpg')
     commentDiv.appendChild(commentImg);
 
-    // create text container div and append it to comment div
+    /* displayComment function: create text container div and append it to comment div */
     let innerDiv = document.createElement('div')
     innerDiv.classList.add('comments__inner-container')
     commentDiv.appendChild(innerDiv)
 
-    // create commentText div and append it to commentDiv
+    /* displayComment function: create commentText div and append it to commentDiv */
     let commentText = document.createElement('div');
     commentText.classList.add('comments__text');
     innerDiv.appendChild(commentText);
 
-    //create meta date div and append it to commentDiv
+    /* displayComment function: create meta date div and append it to commentDiv */
     let commentMeta = document.createElement('div');
     commentMeta.classList.add('comments__meta');
     commentText.appendChild(commentMeta);
 
-    // create name span and append it to comment meta div 
+    /* displayComment function: create name span and append it to comment meta div */
     let commentName = document.createElement('span');
     commentName.classList.add('comments__name');
     commentName.innerText = comment.name;
     commentMeta.appendChild(commentName);
 
-    // create date span and append it to meta div
+    /* displayComment function: create date span and append it to meta div */
     let commentDate = document.createElement('span');
     commentDate.classList.add('comments__date');
     commentDate.innerText = comment.date;
     commentMeta.appendChild(commentDate);
 
-    // create comment body div and append it to innerDiv
+    /* displayComment function: create comment body div and append it to innerDiv */
     let commentBody = document.createElement('div');
     commentBody.classList.add('comments__text');
     innerDiv.appendChild(commentBody);
 
-    // create comment text para and append it to comment Body 
+    /* displayComment function: create comment text para and append it to comment Body */
     let commentP = document.createElement('p');
     commentP.classList.add('comments__p');
     commentP.innerText = comment.comment;
     commentBody.appendChild(commentP);
     
-    // append the newly created comment div to the comments container
+    /* displayComment function: append the newly created comment div to the comments container */
     commentContainer.appendChild(commentDiv)
 }
 
-// Render default comments to the screen
-
-// Create a comment object using the values from the form inputs
+/* Create a comment object using the values from the form inputs */
 function makeCommentObject(){
     let comment     = {}
     comment.name    = nameInput.value
@@ -70,29 +68,39 @@ function makeCommentObject(){
     return comment
 }
 
-// Clear the comments from the screen, create a new comment and add it to the array, 
+/* Clear the comments from the screen, create a new comment and add it to the array, */
 function submitComment(){
-    commentContainer.innerHTML = '';
-    commentsArray.unshift(makeCommentObject())
-    nameInput.value = ''
-    commentInput.value = '';
-    commentsArray.forEach(c => displayComment(c))
+    axios({
+        method: 'post',
+        url: `${_URL}comments?api_key=${API_KEY}`,
+        data: makeCommentObject()
+      })
+      .then(() => {
+        return axios.get(`${_URL}comments?api_key=${API_KEY}`)
+      })
+      .then(res => {
+        console.log(res)
+        commentContainer.innerHTML = '';
+        nameInput.value = ''
+        commentInput.value = ''; 
+        res.data.forEach(c => displayComment(c))
+    })
+
+.catch(err => console.log(err))
 }
 
-// Add event listener to button
+/* Add event listener to button */
 submitButton.addEventListener('click', e => {
     e.preventDefault()
     submitComment()
 })
 
-// declare a variable to hold the comments returned from axios GET request
-let commentsArray; 
 
-// make axios GET request
+/* make axios GET request to append default comments to the page */
+
 axios.get(`${_URL}comments?api_key=${API_KEY}`)
-    .then(res => {
-        commentsArray = res.data 
-        console.log(commentsArray)
-        commentsArray.forEach(c => displayComment(c))
-    })
-    .catch(err => console.log(err))
+     .then(res => {
+            res.data.forEach(c => displayComment(c))
+        })
+     .catch(err => console.error(err))
+    
